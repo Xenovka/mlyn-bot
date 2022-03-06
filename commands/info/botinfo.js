24b2e563b5f1534b5153e00a04216360f596f08a
Xenovka <wendyanto18@gmail.com>
2022-03-06 15:42:37 +0700

    comamnd: bot info

---
+++ commands/info/botinfo.js
@@ -0,0 +1,55 @@
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
+
+module.exports = {
+  commands: ["botinfo", "boti", "info", "bi"],
+  async callback({ message, client }) {
+    const { channel, createdAt } = message;
+    const bot = await client.application.fetch();
+    const botName = bot.name;
+    const botIcon = bot.iconURL();
+    const botCreatedAt = `${moment(bot.createdAt).format("dddd, MMMM DD, YYYY")} (${moment(bot.createdAt).fromNow()})`;
+    const messageCreatedAt = moment(createdAt).calendar();
+
+    const embed = new MessageEmbed()
+      .setAuthor({ name: botName, iconURL: botIcon })
+      .setThumbnail(bot.iconURL({ type: "png", size: 2048 }))
+      .setColor("BLUE")
+      .addFields([
+        {
+          name: "Creator",
+          value: bot.owner.tag,
+          inline: true
+        },
+        {
+          name: "Version",
+          value: "1.0.0",
+          inline: true
+        },
+        {
+          name: "Library",
+          value: "Discord.js",
+          inline: true
+        },
+        {
+          name: "ID",
+          value: bot.id,
+          inline: false
+        },
+        {
+          name: "Created At",
+          value: botCreatedAt,
+          inline: false
+        },
+        {
+          name: "Invite Link",
+          value:
+            "https://discord.com/api/oauth2/authorize?client_id=948774973726675015&permissions=416594521335&scope=bot%20applications.commands",
+          inline: false
+        }
+      ])
+      .setFooter({ iconURL: botIcon, text: `${botName} • ${messageCreatedAt}` });
+
+    channel.send({ embeds: [embed] });
+  }
+};
