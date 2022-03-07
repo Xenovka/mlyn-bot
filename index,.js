4c04fe6d30444e75de1325a726b1342c45f14be6
Xenovka <wendyanto18@gmail.com>
2022-03-07 20:23:58 +0700

    add stickers and emojis intent

--- index,.js
+++ index,.js
@@ -5,7 +5,12 @@ require("dotenv").config();
 
 const { Client, Intents } = require("discord.js");
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS
+  ]
 });
 
 client.on("ready", () => {
