915c1bfabd54189dc432080fc3576da9cfdba04d
Xenovka <wendyanto18@gmail.com>
2022-03-07 19:12:32 +0700

    new command: memberCount

---
+++ commands/info/memberCount.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require("discord.js");
+const embedFooter = require("../../composable/embedFooter");
+
+module.exports = {
+  commands: ["membercount", "mc"],
+  async callback({ message, client }) {
+    const { channel, guild, createdAt } = message;
+    const { name: guildName, memberCount, maximumMembers } = guild;
+
+    const fetchGuild = await guild.fetch();
+    const onlineMembers = fetchGuild.approximatePresenceCount;
+    const bot = client.user;
+
+    const embed = new MessageEmbed()
+      .setAuthor({ name: guildName, iconURL: guild.iconURL() })
+      .setColor("BLUE")
+      .addFields([
+        {
+          name: "Members",
+          value: memberCount.toString(),
+          inline: false
+        },
+        {
+          name: "Online Members",
+          value: onlineMembers.toString(),
+          inline: false
+        },
+        {
+          name: "Maximum Members",
+          value: new Intl.NumberFormat().format(maximumMembers),
+          inline: false
+        }
+      ])
+      .setFooter(embedFooter(bot, createdAt));
+
+    channel.send({ embeds: [embed] });
+  }
+};
