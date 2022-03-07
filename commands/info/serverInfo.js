60550e1ad5b5610e858d95c7c0d44aa466d7960f
Xenovka <wendyanto18@gmail.com>
2022-03-07 20:24:15 +0700

    serverInfo: showing server info

--- commands/info/serverInfo.js
+++ commands/info/serverInfo.js
@@ -9,15 +9,21 @@ module.exports = {
   commands: ["serverinfo", "si"],
   async callback({ message, client }) {
     const { channel, guild, createdAt } = message;
-    const { name: guildName, memberCount, premiumSubscriptionCount, roles, channels } = guild;
+    const { name: guildName, memberCount, premiumSubscriptionCount, roles, channels, emojis } = guild;
 
     const bot = await client.application.fetch();
     const fetchGuild = await guild.fetch();
     const guildIcon = guild.iconURL();
-    const onlineMembers = fetchGuild.approximatePresenceCount;
     const guildOwner = await guild.fetchOwner();
     const guildRoleCount = roles.cache.size;
+    const onlineMembers = fetchGuild.approximatePresenceCount;
     const channelCount = channels.channelCountWithoutThreads;
+    const emojiCount = emojis.cache.size;
+
+    let guildEmojis = "";
+    emojis.cache.forEach((e) => {
+      guildEmojis += emojis.cache.get(e.id).toString();
+    });
 
     const embed = new MessageEmbed()
       .setAuthor({ name: guildName, iconURL: guildIcon })
@@ -63,8 +69,15 @@ module.exports = {
           name: "Created At",
           value: formatCreatedAt(fetchGuild.createdAt),
           inline: false
+        },
+        {
+          name: `Emoji List [${emojiCount}]`,
+          value: guildEmojis,
+          inline: false
         }
       ])
       .setFooter(embedFooter(bot, createdAt));
+
+    channel.send({ embeds: [embed] });
   }
 };
